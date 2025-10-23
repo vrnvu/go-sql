3ca617adbe365f9a4522658fbd39419652b47fbe
ad <username@users.noreply.github.com>
2025-10-23 09:31:29 +0200

    comments

--- main.go
+++ main.go
@@ -12,27 +12,52 @@ import (
 	"time"
 )
 
+// Query is a row from the input CSV file, representing a single query to be executed
 type Query struct {
 	Hostname  string    `csv:"hostname"`
 	StartTime time.Time `csv:"start_time"`
 	EndTime   time.Time `csv:"end_time"`
 }
 
+// Build transforms the Query struct into the SQL query string
+// We could build the query directly from the .csv file, but a Query struct give us flexibility to add more fields in the future and try different query patterns
 // TODO
 func (q *Query) Build() string {
 	return fmt.Sprintf("SELECT * FROM cpu_usage WHERE hostname = %s AND ts BETWEEN %s AND %s", q.Hostname, q.StartTime, q.EndTime)
 }
 
+// Result is a single query result, containing the worker ID, hostname, request start time, and request end time
 type Result struct {
 	WorkerID         int
 	Hostname         string
 	RequestStartTime time.Time
 	RequestEndTime   time.Time
-	// TODO can we get time to first byte?
+	// TODO enhancements: i.e time to response first byte, response completed, etc.
 	// ResponseStartTime time.Time
 	// ResponseEndTime   time.Time
 }
 
+// WorkerPool is a pool of workers that can execute queries
+// It maintains a map of hostnames to query channels, and a round-robin index for selecting the next worker
+// Strategy: fixed hostname to worker, or else round robin
+// For example:
+// For example: 4 cores, 4 workers, 4 query channels
+// We Map worker to Query channels:
+// queries = [chan string, chan string, chan string, chan string]
+// - worker 0: query channel 0
+// - worker 1: query channel 1
+// - worker 2: query channel 2
+// - worker 3: query channel 3
+// When we receive a query, we map the hostname to the corresponding query channel or else round robin
+// Example:
+// query.hostname = "host1" -> query channel 0
+// query.hostname = "host3" -> query channel 2
+// query.hostname = "host2" -> query channel 1
+// query.hostname = "host1" -> query channel 0
+// query.hostname = "host2" -> query channel 1
+// query.hostname = "host3" -> query channel 2
+// query.hostname = "host4" -> query channel 3
+// query.hostname = "host5" -> query channel 0 // idx % numWorkers
 type WorkerPool struct {
 	numWorkers             int
 	map_hostname_to_worker map[string]chan Query
@@ -41,6 +66,7 @@ type WorkerPool struct {
 	queries                []chan Query
 }
 
+// NewWorkerPool creates a new WorkerPool with the given number of workers
 func NewWorkerPool(numWorkers int) *WorkerPool {
 	queries := make([]chan Query, numWorkers)
 	for i := range numWorkers {
@@ -56,6 +82,7 @@ func NewWorkerPool(numWorkers int) *WorkerPool {
 	}
 }
 
+// RunWorkers starts the workers in the WorkerPool
 func (wp *WorkerPool) RunWorkers(ctx context.Context) {
 	for i := 0; i < wp.numWorkers; i++ {
 		go worker(ctx, i, wp.queries[wp.last_worker_idx], wp.results)
@@ -63,8 +90,8 @@ func (wp *WorkerPool) RunWorkers(ctx context.Context) {
 	}
 }
 
-// TODO doc thread safety: last_worker_idx is not thread safe
-// RunQuery must be run sequentially
+// RunQuery allocates a worker to run a query on the WorkerPool
+// Thread safety: `last_worker_idx` is not thread safe
 func (wp *WorkerPool) RunQuery(ctx context.Context, query Query) error {
 	if query_worker, ok := wp.map_hostname_to_worker[query.Hostname]; ok {
 		return runQuery(ctx, query_worker, query)
@@ -86,13 +113,13 @@ func runQuery(ctx context.Context, query_worker chan Query, query Query) error {
 }
 
 // TODO aggregate results
-// # of queries processed,
-// total processing time across all queries,
-// the minimum query time (for a single query),
-// the median query time,
-// the average query time,
-// and the maximum query time.
-func (wp *WorkerPool) RunResults(ctx context.Context, done chan<- bool) {
+// - # of queries processed,
+// - total processing time across all queries,
+// - the minimum query time (for a single query),
+// - the median query time,
+// - the average query time,
+// - and the maximum query time.
+func (wp *WorkerPool) CollectResults(ctx context.Context, done chan<- bool) {
 	num_queries := 0
 	for {
 		select {
@@ -110,6 +137,8 @@ func (wp *WorkerPool) RunResults(ctx context.Context, done chan<- bool) {
 	}
 }
 
+// Close all the query channels and the results channel in order to terminate the workers
+// Thread safety: all the channels are closed in a single goroutine, so there is no race condition
 func (wp *WorkerPool) Close() {
 	for _, query_worker := range wp.queries {
 		close(query_worker)
@@ -118,6 +147,7 @@ func (wp *WorkerPool) Close() {
 	close(wp.results)
 }
 
+// TODO
 func worker(ctx context.Context, id int, queries <-chan Query, results chan<- Result) {
 	for {
 		select {
@@ -175,24 +205,7 @@ func main() {
 		log.Fatalf("expected fields to be hostname, start_time, end_time, got %v", fields)
 	}
 
-	// Worker pool, that reads queries channel
-	// Loadbalancing strategy is: fixed hostname to worker, or else round robin
-	// 4 cores, 4 workers, 4 query channels
-	// queries = [chan string, chan string, chan string, chan string]
-	// worker 0: query channel 0
-	// worker 1: query channel 1
-	// worker 2: query channel 2
-	// worker 3: query channel 3
-	// Example:
-	// query.hostname = "host1" -> query channel 0
-	// query.hostname = "host3" -> query channel 2
-	// query.hostname = "host2" -> query channel 1
-	// query.hostname = "host1" -> query channel 0
-	// query.hostname = "host2" -> query channel 1
-	// query.hostname = "host3" -> query channel 2
-	// query.hostname = "host4" -> query channel 3
-	// query.hostname = "host5" -> query channel 0 // idx % numWorkers
-
+	// TODO Since this is IO bound to Network, I'd consider a timeout
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -200,7 +213,7 @@ func main() {
 	wp.RunWorkers(ctx)
 
 	done := make(chan bool)
-	go wp.RunResults(ctx, done)
+	go wp.CollectResults(ctx, done)
 
 	for {
 		select {
