7e904fc8f3f16437a60f5714cbc3c80e26c6c6f5
ad <username@users.noreply.github.com>
2025-10-23 15:19:39 +0200

    reservoir snaps

--- internal/metrics/reservoir_test.go
+++ internal/metrics/reservoir_test.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"testing"
+	"time"
 
 	"github.com/gkampitakis/go-snaps/snaps"
 	"github.com/stretchr/testify/assert"
@@ -29,4 +30,44 @@ func TestNewReservoirWithSizeTooManySize(t *testing.T) {
 	assert.Error(t, err)
 	assert.Nil(t, metrics)
 	snaps.MatchSnapshot(t, err.Error())
+}
+
+func TestReservoirAddResponseDoesNotPanicWhenMaxCapacityIsReached(t *testing.T) {
+	rapid.Check(t, func(t *rapid.T) {
+		capacity := rapid.IntRange(1, ReservoirMaxCapacity).Draw(t, "capacity")
+		metrics, err := NewReservoirWithSize(capacity)
+		assert.NoError(t, err)
+		assert.NotNil(t, metrics)
+
+		for range capacity {
+			metrics.AddResponse(time.Duration(1) * time.Second)
+		}
+
+		assert.NotPanics(t, func() {
+			metrics.AddResponse(1 * time.Second)
+		})
+	})
+}
+
+func TestReservoirMetricsAggregate(t *testing.T) {
+	metrics := NewReservoir()
+	metrics.AddResponse(1 * time.Second)
+	metrics.AddResponse(2 * time.Second)
+	metrics.AddResponse(3 * time.Second)
+	metrics.AddResponse(4 * time.Second)
+	metrics.AddResponse(5 * time.Second)
+	metrics.AddResponse(6 * time.Second)
+	metrics.AddResponse(7 * time.Second)
+	metrics.AddResponse(8 * time.Second)
+	metrics.AddResponse(9 * time.Second)
+	metrics.AddResponse(10 * time.Second)
+
+	result := metrics.Aggregate()
+	assert.Equal(t, 10, result.NumberOfQueries)
+	assert.Equal(t, 55*time.Second, result.TotalProcessingTime)
+	assert.Equal(t, 1*time.Second, result.MinResponse)
+	assert.Equal(t, 6*time.Second, result.MedianResponse)
+	assert.Equal(t, 55*time.Second/10, result.AverageResponse)
+	assert.Equal(t, 10*time.Second, result.MaxResponse)
+	snaps.MatchSnapshot(t, result)
 }
